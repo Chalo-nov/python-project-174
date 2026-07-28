da979423a377d4151f2cc877f9f8f3cd625865ef
chalo-nov <jcn1708@gmail.com>
2026-07-27 20:46:03 -0500

    Add gendiff script and project configuration

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
