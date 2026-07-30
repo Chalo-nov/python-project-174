515837036e8a57304496e82515a0684fd47e0c01
chalo-nov <jcn1708@gmail.com>
2026-07-29 19:30:30 -0500

    Add format argument to CLI

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,7 @@ def main():
     )
     parser.add_argument('first_file', help='First file to compare')
     parser.add_argument('second_file', help='Second file to compare')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
 
 
