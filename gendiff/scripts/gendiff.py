e7ceccb30acee6edf6ca64325432267011a84c46
chalo-nov <jcn1708@gmail.com>
2026-08-26 19:11:05 -0500

    Add support for plain and json formatters in generate_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,18 +1,25 @@
 import argparse
-from gendiff import generate_diff
+from gendiff.gendiff import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file', help='First file to compare')
-    parser.add_argument('second_file', help='Second file to compare')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='set format of output (default: "stylish")'
+    )
 
-    result = generate_diff(args.first_file, args.second_file)
+    args = parser.parse_args()
+    
+    # Asegúrate de pasar args.format como tercer parámetro
+    result = generate_diff(args.first_file, args.second_file, args.format)
     print(result)
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == '__main__':
+    main()
